3d535ce9a7c786c7988ff2afc17598abae571679
vinivin153 <vinivin153@naver.com>
2024-10-24 15:20:56 +0900

    feat: ProtectedRoute 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { Home } from './pages/main/Home';
+import { ProtectedRoute } from './components/ProtectedRoute.jsx';
+import { Mission } from './pages/mission/Mission.jsx';
+import { Header } from './components/header/Header.jsx';
+import { PostDetail } from './pages/postdetail/PostDetail.jsx';
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/mission',
+          element: (
+            <ProtectedRoute>
+              <Mission />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/detail',
+          element: <PostDetail />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
