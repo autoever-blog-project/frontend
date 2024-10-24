e28dd5d189125bd3efad0c4d7efb8b12aaab9282
vinivin153 <vinivin153@naver.com>
2024-10-24 20:23:24 +0900

    design: 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,11 @@
 const colors = {
   bgBlue: '#a1b8ec',
   mainBlue: '#6b7acc',
+  hashTag: '#404040',
+  lightGray: '#F1F3F5',
+  darkGray: '#999999',
+  mediumGray: '#868E96',
+  deleteRed: '#ff5a4a',
 };
 
 export const theme = {
