f5a5014a641c4d33ce469f8c62b21fcb9691769d
vinivin153 <vinivin153@naver.com>
2024-10-25 20:28:36 +0900

    feat: api 연동

--- src/hooks/usePostDetailQuery.js
+++ src/hooks/usePostDetailQuery.js
@@ -1,25 +1,26 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchLiked, fetchPostDetail } from '../api/detail';
 
-export const usePostDetailInfo = () => {
-  const { data } = useQuery('asdf', fetchPostDetail);
+export const usePostDetailInfo = (postId) => {
+  const { data } = useQuery({ queryKey: ['postDetail'], queryFn: () => fetchPostDetail(postId) });
   return data?.data;
 };
 
-export const useLikedMutation = () => {
+export const useLikedMutation = (postId) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: fetchLiked,
+    mutationFn: () => fetchLiked(postId),
     onMutate: async (postId) => {
       // 요청 보내는도중 새로운 요청이 들어왔을 때 기존 요청 취소
-      await queryClient.cancelQueries('쿼리키');
+      await queryClient.cancelQueries(['postDetail']);
 
       // 요청 실패시 이전 데이터값을 적용해야 하므로 이전 값 가져오기
-      const previousLiked = queryClient.getQueriesData('쿼리키');
+      const previousLiked = queryClient.getQueryData(['postDetail']);
 
       // 새로운 상태 업데이트
-      queryClient.setQueryData('쿼리키', (old) => {
+      queryClient.setQueryData(['postDetail'], (old) => {
+        console.log(old);
         if (!old) return;
         return {
           ...old,
@@ -32,11 +33,11 @@ export const useLikedMutation = () => {
     },
     // 실패시 이전 상태로 돌아감
     onError: (err, userId, context) => {
-      queryClient.setQueryData('쿼리키', context.previousLiked);
+      queryClient.setQueryData(['postDetail'], context.previousLiked);
     },
     // 데이터 동기화
     onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: '쿼리키' });
+      queryClient.invalidateQueries({ queryKey: ['postDetail'] });
     },
   });
 };
